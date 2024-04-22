2b31ec93c0009873dea121523478dcb9f3c5bfed
real-Sandip-Das <sd13535@outlook.com>
2024-04-22 19:47:11 +0530

    Made OpenCV work with CMake successfully on Windows!

--- cpp_files/src/main.cpp
+++ cpp_files/src/main.cpp
@@ -2,15 +2,28 @@
 #include "../include/AStar.h"
 #include <iostream> //debug
 
+using namespace cv;
+
 // The purpose of this file is to debug the A* implementation
 
 int main()
 {
+	const Mat image = imread("C:/Users/sandi/Downloads/lena.jpg");
+	if ( !image.data )
+	{
+		std::cout << "No Image data" << std::endl;
+		return -1;
+	}
+	namedWindow("An Image", WINDOW_AUTOSIZE );
+	imshow("An Image", image);
+	waitKey(0);
+	destroyAllWindows();
 	std::vector<std::vector<int>> map_arr = {
 	    {W, W, W, W},
 	    {W, W, W, W},
 	    {W, W, W, W},
-	    {W, W, W, W}};
+	    {W, W, W, W}
+	};
 	std::pair<std::list<std::pair<int, int>>, double> a;
 	a = cppfiles::a_star(map_arr, 0, 0, 3, 3);
 	std::cout << a.second << std::endl;
